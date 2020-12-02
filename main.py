1e2039d7b03a0495f76af7f55212bd6429ca8f34
ZinedineR <zinedin505@gmail.com>
2020-12-02 21:25:47 +0700

    organizing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-con = sqlite3.connect('db.sqlite')
+con = sqlite3.connect('properties/db.sqlite')
 cursor = con.cursor()
 
 def insert_penghuni():
@@ -19,3 +19,31 @@ def select_penghuni():
 
 select_penghuni()
 #insert_penghuni()
+
+# class DataManager:
+# 	def __init__(self):
+# 		self.con = sqlite3.connect('properties/db.sqlite')
+# 		self.cursor = self.con.cursor() # instantiate a cursor obj
+# 	def executeQuery(self, query, retVal=False):
+# 		self.cursor.execute(query)
+# 		all_results = self.cursor.fetchall()
+# 		self.con.commit()
+# 		if retVal:
+# 			return all_results
+        
+# class Astri(DataManager):
+# 	def setDataPerson(self, nama, email):
+# 		self.query = 'INSERT INTO Person (nama, email) \
+# 			VALUES (\'%s\', \'%s\')' 
+# 		self.query = self.query % (nama, email)
+# 		print('self.query : ', self.query )
+# 		self.executeQuery(self.query)
+# 		
+# 	def getDataPerson(self, nama, email):
+# 		self.query = 'SELECT id_person FROM Person \
+# 			where nama=\'%s\' and email=\'%s\' ' 
+# 		self.query = self.query % (nama, email)
+# 		print('self.query : ', self.query )
+# 		id_person = self.executeQuery(self.query, retVal=True)
+# 		return id_person
+
