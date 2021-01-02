d977af5b95d57fd2970e8c790f98b613e861d480
ZinedineR <zinedin505@gmail.com>
2021-01-02 11:54:55 +0700

    fix again and new cd

--- Post.py
+++ Post.py
@@ -3,32 +3,23 @@ from model import Model
 class jadwal(Model):
     def __init__(self):
         super().__init__("jadwal",["jadwal_id","tangki_id","kategori_id","lorong_id","tangki_tanggal"])
-
-class fiturJadwal(Model):
-    def __init__(self):
+    def fiturjadwal(self):
         super().__init__("jadwal",["tangki_id","kategori_id","lorong_id","tangki_tanggal"])
-
 
 class tangki(Model):
     def __init__(self):
         super().__init__("tangki",["tangki_id","tangki_tanggal","jumlahairliter"])
-
-class tangki_liter(Model):      
-    def __init__(self):
+    def liter(self):
         super().__init__("tangki","jumlahairliter")
+    def historytangki(self):
+        super().__init__("history_tangki","pass")
         
-class historytangki(Model):
-    def __init__(self):
-        super().__init__("history_tangki","pass")        
-
 class penggunaan(Model):
     def __init__(self):
         super().__init__("penggunaan",["penggunaan_id","penghuni_id","kategori_id","tanggal_penggunaan"])
-
-class fiturPenggunaan(Model):
-    def __init__(self):
+    def fiturpenggunaan(self):
         super().__init__("penggunaan",["penghuni_id","kategori_id","tanggal_penggunaan"])
-        
+     
 class lorong(Model):
     def __init__(self):
         super().__init__("lorong",["lorong_id","penghuni_id","namapenghuni"])
@@ -79,7 +70,8 @@ class userview():
         print("===========================")
     
     def showHistoryTangki():
-        model = historytangki()
+        model = tangki()
+        model.historytangki()
         data = model.view()
         print("===========================")
         print("History ID\t\tTangki ID\t\tTanggal Tangki\t\tJumlah air liter")
@@ -102,11 +94,12 @@ class userview():
         print("===========================")
         print("Jadwal ID\t\tTangki ID\t\tkategori ID\t\tLorong ID\t\ttangki_tanggal")
         for n in data:
-            print("\t\t",n[0],"\t\t",n[1],"\t\t",n[2],"\t\t",n[3])
+            print("\t\t",n[0],"\t\t",n[1],"\t\t",n[2],"\t\t",n[3],"\t\t",n[4])
         print("===========================")
         
     def updateTangki():
-        model = tangki_liter()
+        model = tangki()
+        model.liter()
         userview.showTangki()
         keyword = int(input("Inputkan id tangki yang ingin di isi : "))
         values = int(input("tambahkan jumlah liter (gunakan - jika ingin mengurangi) : "))
@@ -114,7 +107,8 @@ class userview():
         model.update(values,wherekey,keyword)
         
     def insertPenggunaan():
-        model = fiturPenggunaan()
+        model = penggunaan()
+        model.fiturpenggunaan()
         userview.showPenghuni()
         idpenghuni = int(input("Inputkan id penghuni anda: "))
         userview.showKategori()
@@ -123,7 +117,8 @@ class userview():
         model.insert([idpenghuni,idkategori,tanggal])
         
     def insertJadwal():
-        model = fiturJadwal()
+        model = jadwal()
+        model.fiturjadwal()
         userview.showTangki()
         idtangki = int(input("Inputkan tangki yang akan digunakan : "))
         userview.showKategori()
